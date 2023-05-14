627afae51cb95d6cfe73c1686e00146efc30cbec
hfjxjjd123 <licbusiness21@gmail.com>
2023-05-14 15:11:39 +0900

    feat: SplashScreen추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:prayhelper/main.dart';
+import 'package:prayhelper/pray_helper_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PrayHelperApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
