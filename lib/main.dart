35098190c4ed86dba18a524230c6aa8d987c0bce
hfjxjjd123 <licbusiness21@gmail.com>
2023-05-17 15:32:46 +0900

    feat: device id 반환 함수구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:prayhelper/func/getDeviceId.dart';
 import 'package:prayhelper/pray_helper_app.dart';
 
-void main() {
+void main() async{
   runApp(const PrayHelperApp());
 }
 
