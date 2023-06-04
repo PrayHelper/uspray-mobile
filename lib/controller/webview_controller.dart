730fd5ab02ab8c39a2d4c339538385ec17c5ac84
hfjxjjd123 <licbusiness21@gmail.com>
2023-06-04 12:39:16 +0900

    feat: Login 통신 코드 완성

--- lib/controller/webview_controller.dart
+++ lib/controller/webview_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,12 +14,9 @@ class WebviewMainController extends GetxController {
     ..setBackgroundColor(const Color(0x00000000))
     ..setNavigationDelegate(
       NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
+        onProgress: (int progress) {},
         onPageStarted: (String url) {},
-        onPageFinished: (String url) {
-        },
+        onPageFinished: (String url) {},
         onWebResourceError: (WebResourceError error) {},
         onNavigationRequest: (NavigationRequest request) async{
           //TODO 특정 url을 따른 로직을 핸들링할 수 있음
@@ -29,26 +25,25 @@ class WebviewMainController extends GetxController {
       ),
     )
     ..addJavaScriptChannel(
-        "TestToaster",
+        "LoginToaster",
         onMessageReceived: (JavaScriptMessage message) async {
           var data = jsonDecode(message.message);
-          logger.d(data['loginSuccess']);
-          //TODO login 성공메시지 명시
-          if(data['loginSuccess']){
-            //TODO sendDeviceToken 로직
+          logger.d(data['loginRequest']);
+          //요청에 대한 응답
+          if(data['loginRequest']){
             String token = await getDeviceToken();
             sendDeviceToken(token);
           }
         }
     )
-    ..loadRequest(Uri.parse('https://search-music.vercel.app/'));
+    ..loadRequest(Uri.parse('https://www.dev.uspray.kr'));
 
   WebViewController getController() {
     return controller;
   }
+
   static void sendDeviceToken(String token){
-    //TODO 리액트 JS 코드의 함수에 따라 JS 코드 완성하기
-    controller.runJavaScript("window.flutter_inappwebview(\"$token\");");
+    controller.runJavaScript("window.getDeviceToken(\"$token\");");
   }
 
 }
