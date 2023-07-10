7e00fd43c1e5961e60079255a1b87c002eb38c1d
hfjxjjd123 <licbusiness21@gmail.com>
2023-07-10 23:49:38 +0900

    chore: iOS Firebase 코드 연결확인

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:prayhelper/pray_helper_app.dart';
 import 'controller/webview_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Get.put(WebviewMainController());
+  await Firebase.initializeApp();
   sleep(Duration(milliseconds: 200));
   runApp(PrayHelperApp());
 }
