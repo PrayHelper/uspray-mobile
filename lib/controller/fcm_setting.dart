1f0703495b1e84e0a40ea6884055be130cf5e129
hfjxjjd123 <licbusiness21@gmail.com>
2023-07-27 18:39:44 +0900

    chore: remove device info plus

--- lib/controller/fcm_setting.dart
+++ lib/controller/fcm_setting.dart
@@ -5,14 +5,25 @@ import 'package:com.prayhelper.uspray/controller/webview_controller.dart';
 import '../func/logger.dart';
 
 void fcmSetting() async{
-  //추가
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  //알림 권한관리
   NotificationSettings settings = await messaging.requestPermission(
     alert: true,
     badge: true,
     sound: true,
   );
-  logger.d('User granted permission: ${settings.authorizationStatus}');
+
+  //TODO
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
 
   //앱단위가 아니라 main에서 정의하는 메시지 핸들링
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
@@ -31,4 +42,12 @@ void fcmSetting() async{
     // final url = message.data['url'];
     // if (url != null) {}
   });
+
+
+  /////////////////////////////////////////
+  /////////////////////////////////////////
 }
+
+
+
+
