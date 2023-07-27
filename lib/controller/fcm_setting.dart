2f545e892eb23e9ce04c4db38ee243fdcd304d13
hfjxjjd123 <licbusiness21@gmail.com>
2023-07-27 19:20:57 +0900

    FCM iOS 세팅완료

--- lib/controller/fcm_setting.dart
+++ lib/controller/fcm_setting.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:com.prayhelper.uspray/controller/local_notification.dart';
 import 'package:com.prayhelper.uspray/controller/webview_controller.dart';
@@ -6,33 +8,46 @@ import '../func/logger.dart';
 
 void fcmSetting() async{
   FirebaseMessaging messaging = FirebaseMessaging.instance;
-  //알림 권한관리
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
 
-  //TODO
-  await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
+  //사용할 함수 명시
+  void _requestIosPermissions() async {
+    await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+  }
+  void _requestAosPermissions() async{
+    await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
+  //알림 권한관리
+  if (Platform.isIOS) {
+    _requestIosPermissions();
+  } else if (Platform.isAndroid) {
+    _requestAosPermissions();
+  }
 
 
   //앱단위가 아니라 main에서 정의하는 메시지 핸들링
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
     RemoteNotification? notification = message.notification;
     AndroidNotification? android = message.notification?.android;
+
+    //여기는 사실 local notification의 영역임
     if (notification != null && android != null) {
-      logger.d("여기가 Local Notification 영역");
+      logger.d("Android 로컬변환 영역");
       displayLocalNotification(flutterLocalNotificationsPlugin, notification);
     }
+
   });
 
   // Listen to the onMessageOpenedApp stream
@@ -42,6 +57,7 @@ void fcmSetting() async{
     // final url = message.data['url'];
     // if (url != null) {}
   });
+
 
 
   /////////////////////////////////////////
