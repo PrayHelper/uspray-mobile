d2356f08876b1e0902af5da5a1054e71e84ddbfd
hfjxjjd123 <licbusiness21@gmail.com>
2023-09-07 10:41:20 +0900

    Add invokeDefault.dart

--- lib/bloc/link_sharing/invokeDefalult.dart
+++ lib/bloc/link_sharing/invokeDefalult.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
+
 import 'package:com.prayhelper.uspray/debug/logger.dart';
 import 'package:flutter/services.dart';
 
 class InvokeDefault {
-  static const platform =
-      const MethodChannel('com.prayhelper.uspray/invokeDefault');
+  static const channel =
+      MethodChannel("com.prayhelper.uspray/invokeDefault");
 
   static Future<void> openManageDefaultAppsSettings() async {
     try {
-      await platform.invokeMethod('openManageDefaultAppsSettings');
+      if(Platform.isAndroid){
+        await channel.invokeMethod('openManageDefaultAppsSettings');
+      }
     } on PlatformException catch (e) {
       logger.d('Error: $e');
     } catch (e){
