3de9daa896abc49f915cd4205ca802e24b27100d
hfjxjjd123 <licbusiness21@gmail.com>
2023-09-10 17:29:24 +0900

    add Handling intent but not work

--- android/app/src/main/kotlin/com/prayhelper/uspray/MainActivity.kt
+++ android/app/src/main/kotlin/com/prayhelper/uspray/MainActivity.kt
@@ -7,10 +7,34 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.prayhelper.uspray/invokeDefault"
     private val packageName = "com.prayhelper.uspray"
+
+    ///
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        handleIntent(intent)
+//    }
+//
+//    override fun onNewIntent(intent: Intent) {
+//        super.onNewIntent(intent)
+//        handleIntent(intent)
+//    }
+//
+//    private fun handleIntent(intent: Intent) {
+//        val appLinkAction = intent.action
+//        val appLinkData: Uri? = intent.data
+//        if (Intent.ACTION_VIEW == appLinkAction) {
+//            appLinkData?.lastPathSegment?.also { recipeId ->
+//                println("For Test")
+//            }
+//        }
+//    }
+    ///
+
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
