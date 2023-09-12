74f76e5a2ef2c5cceb7839ea7b7aec455619babb
hfjxjjd123 <licbusiness21@gmail.com>
2023-09-12 19:12:55 +0900

    main init Kakao share

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:com.prayhelper.uspray/pray_helper_app.dart';
+import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
 import 'bloc/link_sharing/deeplinking.dart';
 import 'bloc/notification/init_notification.dart';
 import 'controller/webview_controller.dart';
@@ -21,5 +22,11 @@ void main() async {
   await initNotification();
   await initDeepLinks();
   // await InvokeDefault.openManageDefaultAppsSettings();
+
+  // For Kakao
+  KakaoSdk.init(
+    nativeAppKey: '{YOUR_NATIVE_APP_KEY}',
+    // javaScriptAppKey: '{YOUR_JAVASCRIPT_APP_KEY}',
+  );
   runApp(PrayHelperApp());
 }
