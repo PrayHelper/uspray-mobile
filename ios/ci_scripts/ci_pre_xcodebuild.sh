f7589d9a1a5f301a1b8512566655fc589cfe8ce3
hfjxjjd123 <licbusiness21@gmail.com>
2023-11-08 23:39:43 +0900

    ci_pre

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,6 +4,9 @@
 #  Runner
 #
 #  Created by 이학림 on 2023/11/08.
-#  
+#
+echo "start ci_pre_xcodebuild.sh"
 brew install cocoapods
+echo "after cocoapods install"
 pod install
+echo "after pod install"
